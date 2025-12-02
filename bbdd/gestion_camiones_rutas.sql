0e2a0d82644d4bf84183cf501f4a39f56be4bf52
CarlesLlaurado <carlesllauradollaurado@gmail.com>
2025-12-02 09:53:56 +0100

    bbdd actualizada

---
+++ bbdd/gestion_camiones_rutas.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for macos15 (arm64)
+--
+-- Host: localhost    Database: gestion_camiones
+-- ------------------------------------------------------
+-- Server version	9.4.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `rutas`
+--
+
+DROP TABLE IF EXISTS `rutas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rutas` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `camion_id` int unsigned NOT NULL,
+  `distancia` int NOT NULL,
+  `duracion` int DEFAULT NULL,
+  `ubicacion` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ubicacion_recogida` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ubicacion_entrega` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `recogida_lat` decimal(10,8) DEFAULT NULL,
+  `recogida_lng` decimal(11,8) DEFAULT NULL,
+  `entrega_lat` decimal(10,8) DEFAULT NULL,
+  `entrega_lng` decimal(11,8) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_FFC3AEF03A706D3` (`camion_id`),
+  CONSTRAINT `FK_FFC3AEF03A706D3` FOREIGN KEY (`camion_id`) REFERENCES `camiones` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rutas`
+--
+
+LOCK TABLES `rutas` WRITE;
+/*!40000 ALTER TABLE `rutas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rutas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-02  9:52:44
